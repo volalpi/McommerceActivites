e458cec00658334f26ced395b8b6447617dd6acc
manantsoa <manantsoa@wylog.com>
2019-03-05 15:42:12 +0300

    Ajout du module microservice-expedition

---
+++ microservice-expedition/src/main/java/com/mexpedition/microserviceexpedition/MicroserviceExpeditionApplication.java
@@ -0,0 +1,13 @@
+package com.mexpedition.microserviceexpedition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceExpeditionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceExpeditionApplication.class, args);
+	}
+
+}
---
+++ microservice-expedition/src/test/java/com/mexpedition/microserviceexpedition/MicroserviceExpeditionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mexpedition.microserviceexpedition;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceExpeditionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
